6150eca0431113d63f6b0c8d80d6ff719d3e56fd
Jay Lee <jay@provire.com>
2020-02-20 07:05:50 -0800

    add name

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --init -v roon-data:/mnt/roon-data -p 9003:9003/udp -p 9100-9200:9100-9200/tcp --network bridge --device /dev/snd roon-server 
+docker run --rm --init -v roon-data:/mnt/roon-data -p 9003:9003/udp -p 9100-9200:9100-9200/tcp --network bridge --device /dev/snd --name roon-server roon-server
